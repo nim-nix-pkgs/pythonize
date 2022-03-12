b71f39e1c771268280daf711df56e9413de23a9f
hugosenari <hugosenari@gmail.com>
2022-03-12 04:10:25 -0300

    chore: re index pythonize

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A higher-level wrapper for the Python Programing Language'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."pythonize-master".url = "path:./master";
+  inputs."pythonize-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."pythonize-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''A higher-level wrapper for the Python Programing Language'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-pythonize-master.flake = false;
+  inputs.src-pythonize-master.owner = "marcoapintoo";
+  inputs.src-pythonize-master.ref   = "refs/heads/master";
+  inputs.src-pythonize-master.repo  = "nim-pythonize";
+  inputs.src-pythonize-master.type  = "github";
+  
+  inputs."python".dir   = "nimpkgs/p/python";
+  inputs."python".owner = "riinr";
+  inputs."python".ref   = "flake-pinning";
+  inputs."python".repo  = "flake-nimble";
+  inputs."python".type  = "github";
+  inputs."python".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."python".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-pythonize-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-pythonize-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
